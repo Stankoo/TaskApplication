90c55d04634f73c5486a73f06d5e2509cbff1de0
Pawel <pawel.stankiewicz95@gmail.com>
2021-08-20 13:20:27 +0200

    task 22.2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,2 +1,33 @@
-package com.crud.tasks.controller;public class TrelloController {
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/v1/trello")
+@RequiredArgsConstructor
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.stream()
+                .filter(trelloBoardDto -> Objects.nonNull(trelloBoardDto.getId())
+                        && Objects.nonNull(trelloBoardDto.getName()))
+                .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
+                .forEach(trelloBoardDto -> {
+                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                });
+    }
 }
--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,2 +1,9 @@
-package com.crud.tasks.domain;public class TrelloBoardDto {
+package com.crud.tasks.domain;
+
+import lombok.Data;
+
+@Data
+public class TrelloBoardDto {
+    private String name;
+    private String id;
 }
